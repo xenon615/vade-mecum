5f6e3e9f3699c26d4a81d0a0f76cd273bbfb6117
xenon615@gmail.com <xenon615@limeira.eu>
2021-03-18 20:37:47 +0200

    target

---
+++ Find.lua
@@ -0,0 +1,52 @@
+local addonName, vm = ...
+local findForm 
+--local functions
+local creteForm
+vm.Find = {
+    form = function()
+        if findForm == nil then
+            createForm()
+        end
+        findForm:Show()
+    end
+}
+
+function createForm()
+    findForm = CreateFrame('Frame', 'VadeMecum_Find', UIParent)
+    findForm:SetPoint('CENTER')
+    findForm:SetSize(260, 50)
+    findForm:SetBackdrop(vm.Config.backDrop)
+    findForm:SetBackdropColor(0, 0, 0, 0.7)
+
+    local targetName = CreateFrame("EditBox", nil, findForm, 'InputBoxTemplate')
+    targetName:SetSize(160, 30)
+    targetName:SetPoint('TOPLEFT', 20, -10)
+    targetName:SetScript("OnEscapePressed", function(self)
+        self:GetParent():Hide()
+    end)
+    targetName:SetHistoryLines(10)
+    targetName:Show()
+    local message = findForm:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    
+    message:SetPoint(targetName:GetPoint())
+    message:Hide()
+    local btn = CreateFrame("Button", 'VadeMecum_Find_Go', findForm , 'SecureActionButtonTemplate, UIPanelButtonTemplate')
+    btn:SetPoint('LEFT', targetName, 170, 0)
+    btn:SetSize(60, 30)
+    btn:SetText('Find')
+    btn:SetAttribute("type", "macro")
+    btn:SetScript('PreClick', function()
+        targetName:AddHistoryLine(targetName:GetText())
+        if IsShiftKeyDown() then
+            targetName:Show()
+            message:Hide()
+        else 
+            targetName:Hide()
+            message:SetText('"' .. targetName:GetText() .. '"' .."\n" .. 'shift + click to change')
+            message:Show()
+        end    
+        local mt = "/target " .. targetName:GetText() .. "\n" .."/run local n, r = UnitName('target')  if n then SetRaidTarget('target', 2) end"
+        btn:SetAttribute("macrotext", mt)
+    end)
+    btn:Show()
+end
